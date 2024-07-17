f8dff9013ab0d9a762a7676a48c9bb7cd193fe61
jinqiang zhang <peeweep@0x0.ee>
2024-07-17 09:24:06 +0000

    init
    Signed-off-by: jinqiang zhang <peeweep@0x0.ee>

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# https://forgejo.org/docs/next/admin/actions/#settings
+set -e
+
+mkdir -p data
+touch data/.runner
+mkdir -p data/.cache
+
+chown -R 1001:1001 data/.runner
+chown -R 1001:1001 data/.cache
+chmod 775 data/.runner
+chmod 775 data/.cache
+chmod g+s data/.runner
+chmod g+s data/.cache
